e222344eb758d1a52cfc2c4b476ec32c9000afa2
haruyama8940 <yaruo0406@gmail.com>
2022-09-12 16:52:38 +0900

    fix

---
+++ scripts/intersection_detect_node.py
@@ -0,0 +1,192 @@
+
+#!/usr/bin/env python3
+from __future__ import print_function
+
+from numpy import dtype
+import roslib
+roslib.load_manifest('intersection_detect')
+import rospy
+import cv2
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+from intersection_detect_net import *
+from skimage.transform import resize
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import PoseArray
+from std_msgs.msg import Int8
+from std_srvs.srv import Trigger
+from nav_msgs.msg import Path
+from std_msgs.msg import Int8MultiArray
+from waypoint_nav.msg import cmd_dir_intersection
+from geometry_msgs.msg import PoseWithCovarianceStamped
+from std_srvs.srv import Empty
+from std_srvs.srv import SetBool, SetBoolResponse
+import csv
+import os
+import time
+import copy
+import sys
+import tf
+from nav_msgs.msg import Odometry
+
+class intersection_detect_node:
+    def __init__(self):
+        rospy.init_node('intersection_detect_node', anonymous=True)
+        self.action_num = 4
+        self.dl = deep_learning(n_action = self.action_num)
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber("/camera/rgb/image_raw", Image, self.callback)
+        self.image_left_sub = rospy.Subscriber("/camera_left/rgb/image_raw", Image, self.callback_left_camera)
+        self.image_right_sub = rospy.Subscriber("/camera_right/rgb/image_raw", Image, self.callback_right_camera)
+        self.srv = rospy.Service('/training', SetBool, self.callback_dl_training)
+        self.mode_save_srv = rospy.Service('/model_save', Trigger, self.callback_model_save)
+ #       self.cmd_dir_sub = rospy.Subscriber("/cmd_dir_intersection", Int8MultiArray, self.callback_cmd,queue_size=1)
+        self.cmd_dir_sub = rospy.Subscriber("/cmd_dir_intersection", cmd_dir_intersection, self.callback_cmd,queue_size=1)
+        self.min_distance = 0.0
+        self.action = 0.0
+        self.episode = 0
+        self.vel = Twist()
+        self.path_pose = PoseArray()
+        self.cv_image = np.zeros((480,640,3), np.uint8)
+        self.cv_left_image = np.zeros((480,640,3), np.uint8)
+        self.cv_right_image = np.zeros((480,640,3), np.uint8)
+        self.learning = True
+        self.select_dl = False
+        self.start_time = time.strftime("%Y%m%d_%H:%M:%S")
+        self.path = roslib.packages.get_pkg_dir('intersection_detect') + '/data/result_with_dir_'+str(self.mode)+'/'
+        self.save_path = roslib.packages.get_pkg_dir('intersection_detect') + '/data/model_with_dir_'+str(self.mode)+'/pytorch/'
+        self.load_path =roslib.packages.get_pkg_dir('intersection_detect') + '/data/model_with_dir_'+str(self.mode)+'/test/model_gpu.pt'
+        self.previous_reset_time = 0
+        self.pos_x = 0.0
+        self.pos_y = 0.0
+        self.pos_the = 0.0
+        self.is_started = False
+        self.cmd_dir_data = 0
+        #self.cmd_dir_data = [0, 0, 0]
+        self.start_time_s = rospy.get_time()
+        os.makedirs(self.path + self.start_time)
+
+        with open(self.path + self.start_time + '/' +  'training.csv', 'w') as f:
+            writer = csv.writer(f, lineterminator='\n')
+            writer.writerow(['step', 'mode', 'loss', 'angle_error(rad)', 'distance(m)','x(m)','y(m)', 'the(rad)', 'direction'])
+
+    def callback(self, data):
+        try:
+            self.cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+
+    def callback_left_camera(self, data):
+        try:
+            self.cv_left_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+
+    def callback_right_camera(self, data):
+        try:
+            self.cv_right_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+
+
+    def callback_cmd(self, data):
+        self.cmd_dir_data = data.intersection_label
+
+
+    def callback_dl_training(self, data):
+        resp = SetBoolResponse()
+        self.learning = data.data
+        resp.message = "Training: " + str(self.learning)
+        resp.success = True
+        return resp
+
+    def callback_model_save(self, data):
+        model_res = SetBoolResponse()
+        self.dl.save(self.save_path)
+        model_res.message ="model_save"
+        model_res.success = True
+        return model_res
+
+    def loop(self):
+        if self.cv_image.size != 640 * 480 * 3:
+            return
+        if self.cv_left_image.size != 640 * 480 * 3:
+            return
+        if self.cv_right_image.size != 640 * 480 * 3:
+            return
+        if self.vel.linear.x != 0:
+            self.is_started = True
+        if self.is_started == False:
+            return
+        img = resize(self.cv_image, (48, 64), mode='constant')
+        
+        # r, g, b = cv2.split(img)
+        # img = np.asanyarray([r,g,b])
+
+        img_left = resize(self.cv_left_image, (48, 64), mode='constant')
+        #r, g, b = cv2.split(img_left)
+        #img_left = np.asanyarray([r,g,b])
+
+        img_right = resize(self.cv_right_image, (48, 64), mode='constant')
+        #r, g, b = cv2.split(img_right)
+        #img_right = np.asanyarray([r,g,b])
+        # cmd_dir = np.asanyarray(self.cmd_dir_data)
+        ros_time = str(rospy.Time.now())
+
+        # if self.episode == 0:
+        #     self.learning = True
+        #     #self.dl.save(self.save_path)
+        #     self.dl.load(self.load_path)
+        #     print("load model",self.load_path)
+        
+        if self.episode == 30000:
+            self.learning = False
+            self.dl.save(self.save_path)
+            #self.dl.load(self.load_path)
+        if self.episode == 90000:
+            os.system('killall roslaunch')
+            sys.exit()
+
+        if self.learning:
+
+            intersection , loss = dl.act_and_trains(img , self.cmd_dir_data)
+                
+          
+            # end mode
+
+            print(str(self.episode) + "loss: " + str(loss) + ", label " + str(intersection) + ", correct label: " + str(self.cmd_dir_data))
+            # self.episode += 1
+            # line = [str(self.episode), "training", str(loss), str(angle_error), str(distance), str(self.pos_x), str(self.pos_y), str(self.pos_the), str(self.cmd_dir_data)]
+            # with open(self.path + self.start_time + '/' + 'training.csv', 'a') as f:
+            #     writer = csv.writer(f, lineterminator='\n')
+            #     writer.writerow(line)
+            # self.vel.linear.x = 0.2
+            # self.vel.angular.z = target_action
+            # self.nav_pub.publish(self.vel)
+
+        else:
+            #print('\033[32m'+'test_mode'+'\033[0m')
+            intersection = self.dl.act(img)
+            print(str(self.episode) + ", label:" + str(intersection)  + ", correct label: " + str(self.cmd_dir_data))
+
+            self.episode +=1
+            #line = [str(self.episode), "test", "0", str(angle_error), str(distance), str(self.pos_x), str(self.pos_y), str(self.pos_the), str(self.cmd_dir_data)]
+            # with open(self.path + self.start_time + '/' + 'training.csv', 'a') as f:
+            #     writer = csv.writer(f, lineterminator='\n')
+            #     writer.writerow(line)
+
+        temp = copy.deepcopy(img)
+        cv2.imshow("Resized Image", temp)
+        temp = copy.deepcopy(img_left)
+        cv2.imshow("Resized Left Image", temp)
+        temp = copy.deepcopy(img_right)
+        cv2.imshow("Resized Right Image", temp)
+        cv2.waitKey(1)
+
+if __name__ == '__main__':
+    rg = intersection_detect_node()
+    DURATION = 0.2
+    r = rospy.Rate(1 / DURATION)
+    while not rospy.is_shutdown():
+        rg.loop()
+        r.sleep()
